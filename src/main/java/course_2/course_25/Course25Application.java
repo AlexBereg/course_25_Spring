5e52522ec88c51ddc4630a302a8030cba6ffe445
Alex <berezhnov@gmail.com>
2024-01-10 19:39:20 +0300

    Initial commit

---
+++ src/main/java/course_2/course_25/Course25Application.java
@@ -0,0 +1,13 @@
+package course_2.course_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course25Application.class, args);
+	}
+
+}
